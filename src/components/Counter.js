5eb34ae000dd1d0dcdb6b42b022e047504a53617
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-08 12:25:40 +0600

    adding

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Counter = () => {
-    return (
-        <div>
-           <div>counter</div>     
-        </div>
-    );
+  const [count, setcount] = useState(0);
+
+  const incremnetHandller = () => {
+    setcount((count) => count + 1);
+  };
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h3>count : {count}</h3>
+      <button type="btn" onClick={incremnetHandller}>
+        Increment
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
