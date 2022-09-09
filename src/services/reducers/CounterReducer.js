eefe2b36d7504f2efea9d3ed231dd2e427179a18
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-09 21:53:37 +0600

    adding

--- src/services/reducers/CounterReducer.js
+++ src/services/reducers/CounterReducer.js
@@ -4,7 +4,7 @@
 
 import { increment, deccrement, reset } from "../constants/CounterConstant";
 
-const initialCounter = { count: 0, numberofStudent: 10 };
+const initialCounter = { count: 0};
 const CounterReducer = (state = initialCounter, action) => {
   switch (action.type) {
     case increment:
